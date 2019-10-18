bd88fb1987d8e9d1c6422860f3c4ba40a09c2258
adaliuBC <adaliu1998@163.com>
2019-10-18 09:32:50 +0800

    basic:comp_mean+comp_var; best: train_mean+comp_var; useless: train_beta+train_gamma

--- pygcn/layers.py
+++ pygcn/layers.py
@@ -81,15 +81,55 @@ class NodeNormalization(Module):
         super(NodeNormalization, self).__init__()
 
         self.trans = weight_norm(nn.Linear(in_size, out_size))
+        
+        self.fc1_m = nn.Linear(in_size, (1+in_size)//2)
+        self.relu1_m = nn.ReLU(True)
+        self.fc2_m = nn.Linear((1+in_size)//2, 1)
+        self.relu2_m = nn.ReLU(True)
+        self.net_mean = nn.Sequential(self.fc1_m, self.relu1_m, self.fc2_m, self.relu2_m)
+        
+        self.fc1_v = nn.Linear(in_size+1, (1+in_size)//2)
+        self.relu1_v = nn.ReLU(True)
+        self.fc2_v = nn.Linear((1+in_size)//2, 1)
+        self.relu2_v = nn.ReLU(True)
+        self.net_var = nn.Sequential(self.fc1_v, self.relu1_v, self.fc2_v, self.relu2_v)
+        
+        self.beta_net = nn.Conv1d(in_size, 1, 1)
+        self.gamma_net = nn.Conv1d(in_size, 1, 1)
+        self.train_beta = torch.nn.Parameter(torch.FloatTensor(1))
+        self.train_gamma = torch.nn.Parameter(torch.FloatTensor(1))
+        
 
     def forward(self, input):
+        #comp_mean, comp_var
+        #out_mean = torch.mean(input, 1, keepdim=True).expand_as(input)  #对每行求平均
+        #out_var = torch.mean((input - out_mean+1.0e-10)*(input - out_mean+1.0e-10), 1, keepdim=True)  #对每行求方差
+        #out_var = torch.sqrt(out_var+1.0e-10).expand_as(input)  #求标准差
+        #out = (input-out_mean)/(out_var+1.0e-10)  #减均值除标准差
+        #out = self.trans(out)
 
-        out_mean = torch.mean(input, 1, keepdim=True).expand_as(input)  #对每行求平均
-        out_var = torch.mean((input - out_mean+1.0e-10)*(input - out_mean+1.0e-10), 1, keepdim=True)  #对每行求方差
-        out_var = torch.sqrt(out_var+1.0e-10).expand_as(input)  #求标准差
-        out = (input-out_mean)/(out_var+1.0e-10)  #减均值除标准差
-        out = self.trans(out)
-
+        #train_mean, comp_var
+        row_num = (input.size())[0]
+        train_mean = self.net_mean(input)
+        train_mean = train_mean.expand(row_num, 1)
+        cat = torch.cat((input, train_mean), 1)
+        train_var = self.net_var(cat)
+        comp_mean = torch.mean(input, 1, keepdim=True).expand_as(input)  #对每行求平均
+        comp_var = torch.mean((input - comp_mean+1.0e-10)*(input - comp_mean+1.0e-10), 1, keepdim=True)  #对每行求方差
+        comp_var = torch.sqrt(comp_var+1.0e-10).expand_as(input)  #求标准差
+        out = (input-train_mean)/(train_var+1.0e-10)
+        
+        #train_beta, gamma
+        #comp_mean = torch.mean(input, 1, keepdim=True).expand_as(input)  #对每行求平均
+        #comp_var = torch.mean((input - comp_mean+1.0e-10)*(input - comp_mean+1.0e-10), 1, keepdim=True)  #对每行求方差
+        #comp_var = torch.sqrt(comp_var+1.0e-10).expand_as(input)  #求标准差
+        #comp_in = (input-comp_mean)/(comp_var+1.0e-10)
+        #print(self.train_gamma, self.train_beta)
+        #row_num = (input.size())[0]
+        #train_beta = torch.Tensor(self.train_beta)
+        #train_beta = train_beta.expand(row_num, 1)
+        #out = comp_in*self.train_gamma + self.train_beta
+        
         return out
 
 #如果是对每个feature进行normalization呢？少出现的feature有更多意义……？
